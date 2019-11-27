1cbef278326608dd80ccaab7c6c61a27204c5c77
Xvezda <xvezda@naver.com>
2019-11-27 23:36:20 +0900

    Finish exchanger implementation

--- exchange/money.py
+++ exchange/money.py
@@ -16,71 +16,72 @@ import six
 
 class Exchanger(object):
   def __init__(self, unit=None):
-    if not issubclass(unit.__class__, Money):
+    if not isinstance(unit, Money):
       raise ValueError('unit is not `Money` class instance')
     self._unit = unit
     self._rate = {}
+    self._rate[unit.__class__] = unit
 
   def add(self, currency):
-    try:
-      self._rate[currency.__class__] = currency
-    except (AttributeError, KeyError) as error:
-      raise
-    except:
-      pass
+    self._rate[currency.__class__] = currency
+
+  def update(self, currency):
+    if currency.__class__ not in self._rate.keys():
+      raise Exception('currency not exists')
+    self.add(currency)
 
   def exchange(self, currency, to=None):
     try:
-      from_amount = self._rate[currency.__class__].amount
+      from_amount = self._rate[currency.__class__]
       unit_class = self._unit.__class__
-      unit_amount = currency.amount / from_amount
-    except KeyError:
+      unit_amount = currency / from_amount
+    except KeyError as error:
       raise Exception('currency not defined')
     try:
       if to:
-        to_amount = self._rate[to].amount * unit_amount
+        to_amount = self._rate[to] * unit_amount
         return to(to_amount)
-    except KeyError:
+    except KeyError as error:
       raise Exception('destination currency undefined')
     return unit_class(unit_amount)
 
+  def equal(self, a, b):
+    if not isinstance(a, Money) or not isinstance(b, Money):
+      raise ValueError('all arguments should be `Money` type')
+    return self.exchange(a) == self.exchange(b)
 
-class Money(object):
+
+class Money(float):
+  def __new__(self, amount=1, sign=''):
+    return float.__new__(self, amount)
+
   def __init__(self, amount=1, sign=''):
     self._sign = sign
-    self._amount = amount
 
   def __str__(self):
-    ret = u'{}{}'.format(self._amount, self._sign)
+    ret = u'%.2f%s' % (float(self), self._sign)
     if six.PY2: return ret.encode('utf-8')
     return ret
 
-  def __int__(self):
-    return self._amount
-
-  def __long__(self):
-    return self._amount
-
-  def __float__(self):
-    return float(self._amount)
-
   def __eq__(self, other):
+    if not isinstance(other, self.__class__):
+      if isinstance(other, Money):
+        raise ValueError('`%s` cannot be compared without exchanger'
+                         % other.__class__.__name__)
+      else:
+        raise ValueError('`%s` does not inherited `Money` class')
     return self.__hash__() == hash(other)
 
   def __hash__(self):
-    pass
-
-  @property
-  def amount(self):
-    return self._amount
+    return hash((self.__class__, float(self), self._sign))
 
 
+## @brief Define currencies
+#
+# All currencies should inherits `Money` class.
 class Cent(Money):
   def __init__(self, amount=1):
     super(Cent, self).__init__(amount, sign='¢')
-
-  def __hash__(self):
-    return hash((Cent, self.amount, self.sign))
 
 Penny = Cent
 
@@ -89,23 +90,30 @@ class Dollar(Penny):
   def __init__(self, amount=1):
     super(Cent, self).__init__(amount, sign='$')
 
-  def __hash__(self):
-    return hash((Cent, self.amount, self.sign))
-
 
 class Won(Money):
   def __init__(self, amount=10):
     super(Won, self).__init__(amount, sign='₩')
 
-  def __hash__(self):
-    return hash((Won, self.amount, self.sign))
+KRWon = Won
+
+class JPYen(Money):
+  def __init__(self, amount=10):
+    super(JPYen, self).__init__(amount, sign='¥')
 
 
 def main():
+  # Key currency is dollar
   ex = Exchanger(Dollar())
-  ex.add(Won(1200))
-  result = ex.exchange(Won(1200))
-  print(result)
+  # 100 cent is 1 dollar
+  ex.add(Penny(100))
+  # 1177 won is 1 dollar
+  ex.add(KRWon(1177.50))
+  # 109.11 yen is 1 dollar
+  ex.add(JPYen(109.11))
+
+  result = ex.exchange(KRWon(1000), to=JPYen)
+  print('result:', result)
 
 
 if __name__ == "__main__":
