90a2207cc6d176395846815c3fab1148c853ec72
Xvezda <xvezda@naver.com>
2019-11-29 04:13:18 +0900

    Test python super method

--- python-super/test.py
+++ python-super/test.py
@@ -8,14 +8,19 @@ from __future__ import unicode_literals
 class A(object):
   pass
 
+
 class B(A):
   def __init__(self):
     s = super(B, self)
+    # s = super()
     print('type;', type(s))
-    print('dir:', dir(s))
+    attrs = dir(s)
+    print('dir:', attrs)
+    print('__thisclass__:', s.__thisclass__)
 
 
 def main():
+  # Test class B
   b = B()
 
 
