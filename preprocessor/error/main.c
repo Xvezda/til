34443e8d3fe683a4774ab0f7da4993db6e662f76
Xvezda <xvezda@naver.com>
2019-12-10 01:53:30 +0900

    Testing preprocessor

---
+++ preprocessor/error/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#if 0
+intentionally trying to occurring syntax error
+#endif
+
+#if 0
+#error does it work?
+#endif
+
+int main(int argc, char **argv) {
+    // Do nothing
+
+    return 0;
+}
