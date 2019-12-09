e1da53a2ce69875a547b8ae3199e813536efc36e
Xvezda <xvezda@naver.com>
2019-12-10 05:00:01 +0900

    Change spaces to tabs

--- laboratory/tab-width/main.c
+++ laboratory/tab-width/main.c
@@ -1,22 +1,22 @@
 #include	<stdio.h>
-#define		ASDF 	1
+#define		ASDF	1
 
 
-struct test {
-	// How these looks like?
- 	unsigned 	a 		: 10;
-	unsigned 	abcdef 	: 1;
-	signed		foo 	: 1;
+struct	test	{
+	//Howtheselookslike?
+	unsigned	a		:	10;
+	unsigned	abcdef	:	1;
+	signed		foo		:	1;
 };
 
 
-int main(int argc, char **argv)
+int	main(int	argc,	char	**argv)
 {
-	// What about these?
-	int		a 	= 1;
-	int		foo = 2;
+	//What	aboutthese?
+	int	a	=	1;
+	int	foo	=	2;
 
-	struct	test t;
+	struct	test	t;
 
 	return	0;
 }
