0ff4f1d0a3f424d422605ad5e1690bf292174fc5
Xvezda <xvezda@naver.com>
2019-12-10 15:21:21 +0900

    Testing cmake

--- c-programming/unistd/fork/main.c
+++ c-programming/unistd/fork/main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#if (defined(WIN32) || defined(_WIN32) || \
+     defined(__WIN32__) || defined(__NT__))
+#include <windows.h>
+#else
 #include <unistd.h>
+#endif
+
 
 #define PRINT_MSG(src, id) \
     printf(#src ": %d\n", id)
@@ -9,6 +15,7 @@ int main(void)
 {
     pid_t pid;
 
+    puts("=Before fork=");
     if((pid=fork()) == 0) {
         // If pid == 0, it's parent process
         PRINT_MSG(parent, pid);
