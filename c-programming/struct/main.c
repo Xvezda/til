1e2beb39435b0c9148f629c453dbd5f4c0509d60
Xvezda <xvezda@naver.com>
2019-12-10 21:23:25 +0900

    Add pseudo-object

--- c-programming/struct/main.c
+++ c-programming/struct/main.c
@@ -6,28 +6,59 @@
  * Below is called ``compound literal''
  * Reference: https://gcc.gnu.org/onlinedocs/gcc/Compound-Literals.html
  */
-#define TEST_OBJ(msg_) &(test_t) { .size = sizeof(msg_), .msg = (char *)msg_ }
+#define OBJECT(type, data, size) \
+    &(obj_t) { \
+        ._type = type, \
+        ._size = size, \
+        ._ptr = (void *)(data) \
+    }
 
 
-typedef struct test_s {
-    size_t size;
-    union {
-        int dummy;
-        char *msg;
-    };
-} test_t;
+typedef enum type_e {
+    TYPE_INT, TYPE_INT_PTR, TYPE_STR, TYPE_ETC
+} type_t;
+
+typedef struct obj_s {
+    type_t _type;
+    size_t _size;
+    void *_ptr;
+} obj_t;
 
 
-static void print_test(test_t *test)
+static void print_obj(obj_t *obj)
 {
-    printf("%s\n", test->msg);
+    char *fmt;
+    switch (obj->_type) {
+    case TYPE_INT:
+    case TYPE_INT_PTR:
+        fmt = "%d\n";
+        break;
+    case TYPE_STR:
+        fmt = "%s\n";
+        break;
+    case TYPE_ETC:
+    default:
+        fmt = "%p\n";
+        break;
+    }
+    if (obj->_type == TYPE_INT_PTR) {
+        printf(fmt, *(int *)obj->_ptr);
+        return;
+    }
+    printf(fmt, obj->_ptr);
 }
 
 
 int main(void)
 {
     // It works... now thats interesting 🤔
-    print_test(TEST_OBJ("foobar"));
+    print_obj(OBJECT(TYPE_STR, "foobar", sizeof("foobar")));
+    char foobar[] = "hello world";
+    print_obj(OBJECT(TYPE_STR, foobar, sizeof(foobar)));
+
+    print_obj(OBJECT(TYPE_INT, 1234, sizeof(1234)));
+    int test = 31337;
+    print_obj(OBJECT(TYPE_INT_PTR, &test, sizeof(&test)));
 
     return 0;
 }
