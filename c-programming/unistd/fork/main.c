017f895188c287612642b2f37a283944dc3dea74
Xvezda <xvezda@naver.com>
2019-12-11 08:11:38 +0900

    Add windows thread implementation

--- c-programming/unistd/fork/main.c
+++ c-programming/unistd/fork/main.c
@@ -1,31 +1,87 @@
 #include <stdio.h>
 #if defined(_WIN32)
 #include <windows.h>
+#include <tchar.h>
+#include <strsafe.h>
 #else
 #include <unistd.h>
 #endif
 
+typedef struct data_s {
+    char *msg;
+    int id;
+} data_t;
 
-#define PRINT_MSG(src, id) \
-    printf(#src ": %d\n", id)
+#if defined(_WIN32)
+DWORD WINAPI print_msg(LPVOID lpParam);
+#else
+void print_msg(data_t *data);
+#endif
 
 
 int main(void)
 {
 #if defined(_WIN32)
-	/* Windows implementation */
+
+    DWORD dwThreadId;
+    HANDLE hThread;
+
+    data_t parentData = { .msg = "parent", .id = 0 };
+    data_t childData = { .msg = "child", .id = 0 };
+
+    print_msg(&parentData);
+
+    hThread = CreateThread(
+        NULL,
+        0,
+        print_msg,
+        &childData,
+        0,
+        &dwThreadId);
+
+    if (hThread != NULL) {
+        WaitForSingleObject(hThread, INFINITE);
+    }
 #else
     pid_t pid;
 
     puts("=Before fork=");
     if((pid=fork()) == 0) {
         // If pid == 0, it's parent process
-        PRINT_MSG(parent, pid);
+        print_msg("parent", pid);
     } else {
-        PRINT_MSG(child, pid);
+        print_msg("child", pid);
     }
 #endif
     return 0;
 }
 
+
+#if defined(_WIN32)
+DWORD WINAPI print_msg(LPVOID lpParam)
+{
+    TCHAR msgBuf[256];
+
+    data_t *data;
+    HANDLE hStdout;
+
+    size_t cchStringSize;
+    DWORD dwChars;
+
+    hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    data = (data_t *)lpParam;
+
+    StringCchPrintf(msgBuf, sizeof(msgBuf), TEXT("%s: %d\n"),
+        data->msg, data->id);
+    (void)StringCchLength(msgBuf, sizeof(msgBuf), &cchStringSize);
+    WriteConsole(hStdout, msgBuf, (DWORD)cchStringSize, &dwChars, NULL);
+
+    return 0;
+}
+#else
+void print_msg(data_t *data)
+{
+    printf("%s: %d\n", data->msg, data->id);
+}
+#endif
 
