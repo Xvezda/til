a081f0ea8bea68c87613ab730762a36deb553f5d
Xvezda <xvezda@naver.com>
2019-12-10 19:27:44 +0900

    Testing compound literal

--- c-programming/struct/main.c
+++ c-programming/struct/main.c
@@ -9,50 +9,17 @@ typedef struct test_s {
 } test_t;
 
 
-test_t *test_new(void);
-void test_init(test_t *ptr);
-void test_del(test_t *ptr);
-static void print_test(test_t *test);
+static void print_test(test_t *test)
+{
+    printf("%s\n", test->msg);
+}
 
 
 int main(void)
 {
-    test_t *v = test_new();
-
-    /* v = &(test_t) { .msg = (char *)"foobar" }; */
-    v = &(test_t) { .msg = strdup("foobar") };
-
-    print_test(v);
-    test_del(v);
+    // It works... now thats interesting 🤔
+    print_test(&(test_t) { .msg = (char *)"foobar" });
 
     return 0;
-}
-
-
-test_t *test_new(void)
-{
-    test_t *ret = malloc(sizeof(*ret));
-    test_init(ret);
-
-    return ret;
-}
-
-
-void test_init(test_t *ptr)
-{
-    ptr->msg = NULL;
-}
-
-
-void test_del(test_t *ptr)
-{
-    free(ptr->msg);
-    free(ptr);
-}
-
-
-static void print_test(test_t *test)
-{
-    printf("%s\n", test->msg);
 }
 
