009329c13caefc87d4df1f2f4bc923d95327a1b2
Xvezda <xvezda@naver.com>
2019-12-14 19:10:03 +0900

    Add array header

---
+++ cpp-stl-implement/xvzd_array.h
@@ -0,0 +1,19 @@
+#ifndef XVZD_ARRAY_H_
+#define XVZD_ARRAY_H_
+
+#include "xvzd_object.h"
+
+namespace xvzd {
+
+
+class Array : public Object {
+public:
+  Array() {}
+  ~Array() {}
+private:
+};
+
+
+}  // namespace xvzd
+
+#endif  // XVZD_ARRAY_H_
