ba0381f0c80fc02f728397f0ec25c88ff4c76d99
Xvezda <xvezda@naver.com>
2019-12-14 20:35:02 +0900

    Fix segfault issue

--- cpp-stl-implement/xvzd_string.h
+++ cpp-stl-implement/xvzd_string.h
@@ -2,16 +2,20 @@
 #define XVZD_STRING_H_
 
 
+#include <iostream>
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
 #include <cassert>
+#include <cmath>
 
 #include "xvzd_object.h"
 
 namespace xvzd {
 
-
+/**
+ * TODO: Add exceptions
+ */
 typedef struct StringMeta {
   size_t len;
   char  *ptr;
@@ -25,18 +29,53 @@ public:
   }
 
   String(char other) {
+    String();
     CopyStr(&other, 1);
   }
 
   String(const char* other) {
+    String();
     CopyStr(other);
   }
 
+  String(const String& other, int repeat) {
+    String();
+    *this = String(other.CStr(), repeat);
+  }
+
+  String(const char* other, int repeat) {
+    assert(other != nullptr);
+
+    String();
+    size_t len = std::strlen(other);
+    size_t new_len = len * repeat;
+
+    meta.ptr = new char[new_len+1];
+    meta.ptr[0] = '\0';
+    for (int i = repeat; i > 0; --i) {
+      std::strncat(meta.ptr, other, len);
+    }
+    meta.len = new_len;
+  }
+
+  String(char chr, int repeat) {
+    String();
+    size_t len = 1;
+    size_t new_len = len * repeat;
+
+    meta.ptr = new char[new_len+1];
+    std::memset(meta.ptr, chr, new_len);
+    meta.ptr[new_len] = '\0';
+    meta.len = new_len;
+  }
+
   String(const String& other) {
+    String();
     CopyStr(other);
   }
 
   String(int number) {
+    String();
     size_t len = std::snprintf(nullptr, 0, "%d", number);
     char *tmp = new char[len+1];
     std::snprintf(tmp, len+1, "%d", number);
@@ -44,11 +83,13 @@ public:
 
     CopyStr(tmp, len);
 
+    assert(tmp != nullptr);
     delete[] tmp;
+    tmp = nullptr;
   }
 
   ~String() {
-    if (!meta.ptr && meta.len) {
+    if (meta.ptr && meta.len) {
       Clear();
     }
   }
@@ -59,6 +100,10 @@ public:
 
   const char* CStr() const {
     return meta.ptr;
+  }
+
+  String& Append(const char *other) {
+    return Append(String(other));
   }
 
   String& Append(const String& other) {
@@ -77,6 +122,22 @@ public:
     return *this;
   }
 
+  const String Concat(const String& other) {
+    String cpy(*this);
+    cpy += other;
+    return cpy;
+  }
+
+  const String Repeat(int repeat) {
+    String tmp = String(*this);
+    String result = String();
+
+    for (int cnt = repeat; cnt > 0; --cnt) {
+      result += tmp;
+    }
+    return result;
+  }
+
   String& operator=(const String& other) {
     if (this == &other) return *this;
 
@@ -88,13 +149,22 @@ public:
 
   const String operator+(const String& other) {
     String cpy(*this);
-    cpy += other;
-    return cpy;
+    return cpy.Concat(other);
   }
 
   String& operator+=(const String& other) {
     Append(other);
     return *this;
+  }
+
+  char operator[](int idx) const {
+    if (idx < 0) {
+      size_t len = Length();
+      int new_idx = len - std::abs(idx);
+      if (new_idx < 0) return '\0';
+      return meta.ptr[new_idx];
+    }
+    return meta.ptr[idx];
   }
 
   friend std::ostream& operator<<(std::ostream& os, const String& self) {
@@ -107,6 +177,8 @@ private:
 
   inline void Init() {
     Reset();
+    meta.ptr = new char[1];
+    meta.ptr[0] = '\0';
   }
 
   inline void Reset() {
@@ -115,7 +187,13 @@ private:
   }
 
   inline void DelStr() {
+    if (!meta.ptr) return;
+#ifdef DEBUG
+    std::cout << __FILE__ << ':' << __LINE__
+      << ": deallocate string -> " << CStr() << std::endl;
+#endif
     delete[] meta.ptr;
+    meta.ptr = nullptr;
   }
 
   inline void Clear() {
@@ -124,6 +202,7 @@ private:
   }
 
   void CopyStr(const char *src, size_t size) {
+    if (!src) return;
     meta.ptr = new char[size+1];
 
     assert(meta.ptr != nullptr && src);
@@ -134,11 +213,13 @@ private:
   }
 
   void CopyStr(const char *src) {
+    if (!src) return;
     size_t other_len = std::strlen(src);
     CopyStr(src, other_len);
   }
 
   void CopyStr(const String& other) {
+    if (!other.CStr()) return;
     size_t other_len = other.Length();
     CopyStr(other.CStr(), other_len);
   }
--- cpp-stl-implement/xvzd_string_test.cpp
+++ cpp-stl-implement/xvzd_string_test.cpp
@@ -11,7 +11,7 @@ int main(void) {
     String('c')
   };
 
-  for (auto i = 0; i < 3; ++i) {
+  for (int i = 0; i < 3; ++i) {
     std::cout << str_arr[i] << std::endl;
   }
 
@@ -25,6 +25,22 @@ int main(void) {
 
   std::cout << str + " LOL" << std::endl;
   std::cout << str << std::endl;
+
+  std::cout << String('-', 10) << std::endl;
+
+  for (int i = 0, len = str.Length(); i < len; ++i) {
+    std::cout << str[i] << std::endl;
+  }
+
+  std::cout << String('-', 10) << std::endl;
+
+  str = "asdf";
+  std::cout << str[-1] << str[-2] << std::endl;
+
+  std::cout << String("qwerty", 3) << std::endl;
+  std::cout << String("zxcv").Repeat(5) << std::endl;
+  std::cout << String(String("a")) << std::endl;
+  std::cout << String(String("uiop"), 3) << std::endl;
 
   return 0;
 }
