2cd0eb4f6f7358babb696f3a95e739bcba4e618c
Xvezda <xvezda@naver.com>
2019-12-23 14:23:15 +0900

    Add bigint header

---
+++ cpp/datatype/xvzd_bigint.h
@@ -0,0 +1,18 @@
+#ifndef XVZD_BIGINT_H_
+#define XVZD_BIGINT_H_
+
+#include "xvzd_common.h"
+
+namespace xvzd {
+
+
+class BigInt {
+public:
+  BigInt() {}
+  virtual ~BigInt() {}
+private:
+};
+
+}  // namespace xvzd
+
+#endif  // XVZD_BIGINT_H_
