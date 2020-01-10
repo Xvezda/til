35efa085cbb916e7e2c973d73b3cdc2a173dae51
Xvezda <xvezda@naver.com>
2020-01-10 19:29:09 +0900

    Add output buffer halting test

--- php/output-control/ob_test.php
+++ php/output-control/ob_test.php
@@ -3,5 +3,8 @@
 ob_start();
 echo 'hide this';
 
+/* exit; */ # Halt before cleaning -> showing 'hide this'
+            # So, I guess php flushes output buffer's content
+            # before it ends.
 ob_clean();
 echo 'show this';
