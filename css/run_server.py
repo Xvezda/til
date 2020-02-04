cae6c99f94df34804b72cf193bd57e2bfe0c4078
Xvezda <xvezda@naver.com>
2020-02-04 15:43:51 +0900

    Add run server script

---
+++ css/run_server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+if __name__ == '__main__':
+    import sys
+    if 2 < sys.version_info[0]:
+        import os
+        import http.server as hs
+
+        handler_class = hs.partial(hs.SimpleHTTPRequestHandler,
+                                   directory=os.getcwd())
+        hs.test(HandlerClass=handler_class, port=8000, bind='')
+    else:
+        from SimpleHTTPServer import test
+        test()
+
