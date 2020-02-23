6ad4484b42bb03e65c070898fec668a08cd33fb9
Xvezda <xvezda@naver.com>
2020-02-23 19:40:00 +0900

    Add babel

--- javascript/typescript/greeting.ts
+++ javascript/typescript/greeting.ts
@@ -1,8 +1,28 @@
-function greeting(name: string) {
-  console.log('Hello, ' + name);
+class Employee {
+  fullName: string;
+  constructor(public firstName: string, public lastName: string) {
+    this.fullName = firstName + ' ' + lastName;
+  }
+  /// without public:
+  /// Type 'Employee' is missing the following properties
+  /// from type 'Person': firstName, lastName
+  // constructor(firstName: string, lastName: string) {
+  //   this.fullName = firstName + ' ' + lastName;
+  // }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeting(person: Person) {
+  console.log('Hello, ' + person.firstName + ' ' + person.lastName);
 }
 
 // let user = 'John';
-let user = [1, 2, 3];
+// let user = [1, 2, 3];
+// let user = { firstName: 'John', lastName: 'Doe' };
+let user = new Employee('John', 'Doe');
 
 greeting(user);
