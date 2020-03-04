1065cde1dd54ce49808c08e0f18a6057bf9cafaf
Xvezda <xvezda@naver.com>
2020-03-04 10:11:59 +0900

    Add ES6 tests

---
+++ javascript/es6/index.js
@@ -0,0 +1,8 @@
+/**
+ * Index
+ */
+import test, { foo } from './test.js'
+
+console.log('foo:', foo)
+console.log('sum:', test.sum(1, 2))
+console.log('sub:', test.sub(2, 1))
---
+++ javascript/es6/test.js
@@ -0,0 +1,19 @@
+/**
+ * Test
+ */
+function sum(a, b) {
+  return a + b
+}
+
+export function sub(a, b) {
+  return a - b
+}
+
+export const foo = 'bar'
+
+export default {
+  sum,
+  sub,
+  foo,
+}
+
