d0761501466e318cc2d29ac183ed844660559c9f
Xvezda <xvezda@naver.com>
2020-06-14 09:46:14 +0900

    Add import test

---
+++ python/import/module.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+print('foobar')  # Trigger when imported
