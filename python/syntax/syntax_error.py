be1936f3c5ae376722c51fc2ec14d1d424a2ecb1
Xvezda <xvezda@naver.com>
2020-07-04 12:29:27 +0900

    Add syntax error test

---
+++ python/syntax/syntax_error.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+try:
+    try something
+except SyntaxError:
+    print('will it work?')
