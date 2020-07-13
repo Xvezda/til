7d61f48c030791043c595db394de94d8f0452efd
Xvezda <xvezda@naver.com>
2020-07-14 00:27:43 +0900

    Add assertion handling test

---
+++ python/exception/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+try:
+    assert 0
+except Exception as err:
+    print('what')
