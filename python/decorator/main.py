53059bc1f0dac325e5f6c156112196e081a8f376
Xvezda <xvezda@naver.com>
2020-07-23 19:08:16 +0900

    Add decorator test

---
+++ python/decorator/main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+class MyClass(object):
+    def __init__(self):
+        self._handlers = {}
+
+    def run(self):
+        pass
+
+    def listen(self, event):
+        def wrapper(*args, **kwargs):
+            callback = args[0]
+            if not callable(callback):
+                raise TypeError('callback is not callable')
+            if event in self._handlers:
+                self._handlers[event].append(callback)
+                return
+            self._handlers[event] = [callback]
+        return wrapper
+
+    def emit(self, event, data=None):
+        if event not in self._handlers:
+            return
+        for handler in self._handlers[event]:
+            handler(data)
+
+
+mc = MyClass()
+
+@mc.listen('foo')
+def handle_foo(event):
+    print('event foo fired!')
+
+
+mc.run()
+mc.emit('foo')
