3e56b989f299437a5b1c13057adb964bd0f057ae
Xvezda <xvezda@naver.com>
2020-08-18 00:13:18 +0900

    Add comments

--- javascript/monkeypatch/index.js
+++ javascript/monkeypatch/index.js
@@ -1,7 +1,7 @@
 var foo = require('./foo.js');
 var egg = require('./egg.js');
 
-console.log('before monkeypatch');
+console.log('=== before monkeypatch ===');
 foo.bar();
 egg.ham();
 
@@ -11,6 +11,8 @@ fooExports.bar = function() {
   console.log('this is bar and it is now monkeypatched! :)');
 };
 
-console.log('after monkeypatch');
+// It is possible because; every time `require('./foo.js')` is called,
+// same instance of `exports` is returned.
+console.log('=== after monkeypatch ===');
 foo.bar();
 egg.ham();
