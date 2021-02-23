03648bea2ba29361599384fdde1b33e1900dd157
Xvezda <xvezda@naver.com>
2021-02-23 09:22:13 +0900

    Update pseudo.js

--- javascript/prototype/prototype-chain/pseudo.js
+++ javascript/prototype/prototype-chain/pseudo.js
@@ -4,15 +4,23 @@ const protoEquiv = Symbol.for('__proto__')
 // This will emulate prototype chain lookup
 // `foo.bar` is equivalent to `getprop(foo, 'bar)`
 function getprop(obj, prop) {
-  // Check object itself
-  if (obj[prop]) return obj[prop]
+  try {
+    // Check object itself
+    if (obj[prop]) throw obj[prop]
 
-  // Lookup prototypes
-  let proto = obj[protoEquiv]
-  if (proto === null) return
-  do {
-    if (proto[prop]) return proto[prop]
-  } while ((proto=proto[protoEquiv]) !== null)
+    // Lookup prototypes
+    let proto = obj[protoEquiv]
+    if (proto === null) return
+    do {
+      if (proto[prop]) throw proto[prop]
+    } while ((proto=proto[protoEquiv]) !== null)
+  } catch (o) {
+    // Bind this to obj if callable
+    if (o && o.constructor === Function && prop !== '$constructor') {
+      return o.bind(obj)
+    }
+    return o
+  }
 }
 
 // new ctor(...args) is equivalent to newop(ctor, ...args)
@@ -44,10 +52,13 @@ function funcwrap(func) {
 
 
 // Equivalent to function foo() { ... }
-const foo = funcwrap(function() {
+const foo = funcwrap(function foo() {
   this.bar = 1
 })
 foo.$prototype.baz = 2
+foo.$prototype.hello = function() {
+  return `Hi, i'm ${getprop(this, '$constructor').name}`
+}
 
 
 // o = new foo
@@ -60,3 +71,51 @@ console.log(getprop(o, 'baz'))
 console.assert(getprop(o, 'lol') === undefined)
 // o.constructor
 console.assert(getprop(o, '$constructor') === foo)
+// o.hello()
+console.log(getprop(o, 'hello')())
+
+
+// Equivalent to extends keyword
+function inherits(ctor, superctor) {
+  for (const prop in superctor.$prototype) {
+    ctor.$prototype[prop] = superctor.$prototype[prop]
+  }
+  ctor.$prototype['$constructor'] = ctor
+  ctor.$prototype['$superctor'] = superctor
+}
+
+// Equivalent to super
+function parent(thisArg, supercls, method) {
+  supercls.call(thisArg)
+
+  if (!method) return
+  return supercls.$prototype[method].bind(thisArg)
+}
+
+
+const egg = funcwrap(function egg() {
+  // super()
+  parent(this, foo)
+
+  this.spam = 42
+})
+// class egg extends foo
+inherits(egg, foo)
+
+egg.$prototype.hello = function() {
+  // super.hello()
+  return parent(this, foo, 'hello')() + ' and this is inherted method!'
+}
+
+
+// o2 = new egg
+const o2 = newop(egg)
+// o2.bar
+console.log(getprop(o2, 'bar'))
+// o2.baz
+console.log(getprop(o2, 'baz'))
+// o2.spam
+console.log(getprop(o2, 'spam'))
+// o2.hello()
+console.log(getprop(o2, 'hello')())
+
