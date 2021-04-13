70b35cf83f05e4564914a97d9709e90130537d46
Xvezda <xvezda@naver.com>
2021-04-13 20:17:07 +0900

    Update haskell

--- haskell/baby.hs
+++ haskell/baby.hs
@@ -71,9 +71,9 @@ length' :: (Num b) => [a] -> b
 length' [] = 0
 length' (_:xs) = 1 + length' xs
 
-sum' :: (Num a) => [a] -> a
-sum' [] = 0
-sum' (x:xs) = x + sum' xs
+-- sum' :: (Num a) => [a] -> a
+-- sum' [] = 0
+-- sum' (x:xs) = x + sum' xs
 
 bmiTell :: (RealFloat a) => a -> String
 bmiTell bmi
@@ -144,3 +144,164 @@ describeList' xs = "The list is " ++ what xs
                                [x] -> "a singleton list."
                                xs  -> "a longer list."
 
+-- maximum' :: (Ord a) => [a] -> a
+-- maximum' [] = error "maximum of empty list"
+-- maximum' [x] = x
+-- maximum' (x:xs) = max x (maximum' xs)
+-- maximum' (x:xs)
+--   | x > maxTail = x
+--   | otherwise = maxTail
+--   where maxTail = maximum' xs
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x:replicate' (n-1) x
+
+-- take' :: Int -> [a] -> [a]
+-- take' n xs
+--   | null xs || n <= 0 = []
+--   | otherwise = head xs:take' (n-1) (drop 1 xs)
+
+-- take' :: Int -> [a] -> [a]
+-- take' n xs
+--   | null xs || n <= 0 = []
+--   | otherwise = y:take' (n-1) ys
+--   where (y:ys) = xs
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x: take' (n-1) xs
+
+-- reverse' :: [a] -> [a]
+-- reverse' [] = []
+-- reverse' [x] = [x]
+-- reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y):zip' xs ys
+
+-- elem' :: (Eq a) => a -> [a] -> Bool
+-- elem' _ [] = False
+-- elem' a (x:xs)
+--   | a == x    = True
+--   | otherwise = elem' a xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    quicksort [a | a <- xs, a <= x] ++ [x] ++ quicksort [a | a <- xs, x < a]
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ _ [] = []
+zipWith' _ [] _ = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+-- flip' :: (a -> b -> c) -> (b -> a -> c)
+-- flip' f = g
+--     where g x y = f y x
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f y x = f x y
+
+-- map' :: (a -> b) -> [a] -> [b]
+-- map' _ [] = []
+-- map' f (x:xs) = f x : map' f xs
+
+-- filter' :: (a -> Bool) -> [a] -> [a]
+-- filter' _ [] = []
+-- filter' p (x:xs)
+--   | p x       = x : filter' p xs
+--   | otherwise = filter' p xs
+
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x:xs) =
+    quicksort' (filter (<=x) xs) ++ [x] ++ quicksort' (filter (x<) xs)
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000,99999..])
+    where p x = x `mod` 3829 == 0
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' p (x:xs)
+  | p x       = x : takeWhile' p xs
+  | otherwise = []
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n : chain (n `div` 2)
+  | odd n = n : chain (n*3 + 1)
+
+numLongChains :: (Num a) => a
+numLongChains = fromIntegral (howMany (filter isLong (map chain [1..100])))
+    where howMany = length
+          isLong xs = length xs > 15
+
+numLongChains' :: Int
+numLongChains' = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+addThree' :: (Num a) => a -> a -> a -> a
+-- addThree' x y z = (x+y+z)
+-- addThree' = \x -> \y -> \z -> x + y + z
+addThree' = (\x -> (\y -> (\z -> (x + y + z))))
+
+sum' :: (Num a) => [a] -> a
+-- sum' xs = foldl (\acc x -> acc + x) 0 xs
+-- sum' = foldl (+) 0
+sum' = foldl1 (+)
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' x xs = foldl (\acc y -> if y == x then True else acc) False xs
+
+map' :: (a -> b) -> [a] -> [b]
+-- Too expensive
+-- map' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+map' f xs = foldr (\x acc -> f x : acc) [] xs
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldr1 (\x acc -> if x > acc then x else acc)
+
+reverse' :: [a] -> [a]
+-- reverse' = foldl (\acc x -> x : acc) []
+reverse' = foldl (flip (:)) []
+
+product' :: (Foldable t, Num a) => t a -> a
+-- product' xs = foldr (\x acc -> x * acc) 1 xs
+product' = foldr1 (*)
+
+filter' :: (a -> Bool) -> [a] -> [a]
+-- filter' _ [] = []
+-- filter' p (x:xs)
+--   | p x       = x : filter' p xs
+--   | otherwise = filter' p xs
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+-- foldl' :: Foldable t => (b -> a -> b) -> b -> t a -> b
+foldl' _ a [] = a
+foldl' f a (x:xs) = foldl' f (f a x) xs
+
+-- foldr' _ a [] = a
+-- foldr' f a (x:xs) = foldr' f (f x a) xs
+
+foldr' f a [] = a
+foldr' f a (x:xs) = f x (foldr' f a xs)
+
+scanl' _ a [] = [a]
+scanl' f a (x:xs) = a : scanl' f (f a x) xs
+
+
+-- TODO: How to implement scanr?
--- haskell/experiment.hs
+++ haskell/experiment.hs
@@ -1,3 +1,44 @@
+-- Too imperative
 indexOf :: (Eq a) => [a] -> a -> Int
 xs `indexOf` x = if null found then -1 else head found
     where found = [i | i <- [0 .. (length xs)-1], xs !! i == x]
+
+-- Using recursion
+indexOf' :: (Eq a) => [a] -> a -> Int
+-- Edge case
+indexOf' [] _ = -1
+-- Pattern match
+indexOf' (x:xs) a
+  | x == a = 0
+  | not (a `elem` xs) = -1
+  | otherwise = 1 + indexOf' xs a
+
+toUpperCase :: String -> String
+toUpperCase xs = [if x `elem` lowercases
+                     then uppercases !! (lowercases `indexOf` x)
+                     else x | x <- xs]
+                         where lowercases = ['a' .. 'z']
+                               uppercases = ['A' .. 'Z']
+
+
+-- zip' :: [a] -> [b] -> [(a, b)]
+-- zip' xs ys = [ (xs !! i, ys !! i) | i <- [0 .. (min (length xs) (length ys))-1] ]
+
+split :: String -> Char -> [String]
+split "" _ = []
+split (x:"") c
+  | x == c = ["", ""]
+split s c
+  | last s == c = split (init s) c ++ [""]
+  | otherwise   = t : split remain c
+    where t      = takeWhile (/= c) s
+          remain = drop (length t + 1) s
+
+enumerate :: (Num b) => [a] -> [(b, a)]
+enumerate =
+    foldr (\x acc -> (if null acc then 0 else fst (head acc) + 1, x) : acc) []
+
+primes :: (Integral a, Eq a) => a -> [a]
+primes n = [x | x <- [2..n], foldr (\y acc -> x `mod` y /= 0 && acc) True [2..(x-1)]]
+
+
